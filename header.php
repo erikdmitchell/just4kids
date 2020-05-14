81c9243621db4c7782ff71cd52bb0df862b0ad0b
Erik <erikdmitchell@gmail.com>
2020-05-14 08:55:47 -0400

    cleaned up header and updated styles

--- header.php
+++ header.php
@@ -1,51 +1,28 @@
-<!DOCTYPE html>
-<!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<!doctype html>
 <html <?php language_attributes(); ?>>
-<!--<![endif]-->
-
-  <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <link rel="profile" href="http://gmpg.org/xfn/11" />
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
-    <?php wp_head(); ?>
-  </head>
+    <head>
+    	<meta charset="<?php bloginfo( 'charset' ); ?>">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<link rel="profile" href="https://gmpg.org/xfn/11">
+    
+    	<?php wp_head(); ?>
+    </head>
 
   <body <?php body_class(); ?>>
-
-        <div class="container-fluid primary-nav">
-            <div class="container">
-                <nav class="navbar navbar-default" role="navigation">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".j4k-mobile-menu">
-                            <span class="sr-only"><?php _e( 'Toggle navigation', 'j4k' ); ?></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="">just4kidspediatrics.com</a>
-                                        <?php j4k_header_logo(); ?>
-                    </div>
-                    <div class="collapse navbar-collapse primary-menu">
-                        <?php
-                        wp_nav_menu(
-                            array(
-                                'theme_location' => 'primary',
-                                'container' => false,
-                                'menu_class' => 'nav navbar-nav pull-right',
-                                'fallback_cb' => 'j4k_wp_bootstrap_navwalker::fallback',
-                                'walker' => new j4k_wp_bootstrap_navwalker(),
-                            )
-                        );
-                        ?>
-                    </div> <!-- .primary-menu -->
-                </nav>
-            </div><!-- .container -->
-        </div><!-- .navigation -->
-
+    	<header id="masthead" class="site-header">
+    		<div class="site-branding">
+    			<?php j4k_header_logo(); ?>
+    		</div><!-- .site-branding -->
+    
+    		<nav id="site-navigation" class="main-navigation">
+    			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'j4k' ); ?></button>
+    			<?php
+    			wp_nav_menu(
+    				array(
+                        'theme_location' => 'primary',
+    					'menu_id'        => 'primary-menu',
+    				)
+    			);
+    			?>
+    		</nav><!-- #site-navigation -->
+    	</header><!-- #masthead -->
