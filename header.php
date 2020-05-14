110b8d9e7de40d641b4b0715273eed819ec36d23
Erik <erikdmitchell@gmail.com>
2020-05-14 09:01:56 -0400

    updated logo and nav position

--- header.php
+++ header.php
@@ -9,20 +9,24 @@
     </head>
 
   <body <?php body_class(); ?>>
-    	<header id="masthead" class="site-header">
-    		<div class="site-branding">
-    			<?php j4k_header_logo(); ?>
-    		</div><!-- .site-branding -->
-    
-    		<nav id="site-navigation" class="main-navigation">
-    			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'j4k' ); ?></button>
-    			<?php
-    			wp_nav_menu(
-    				array(
-                        'theme_location' => 'primary',
-    					'menu_id'        => 'primary-menu',
-    				)
-    			);
-    			?>
-    		</nav><!-- #site-navigation -->
-    	</header><!-- #masthead -->
+        <div class="container">
+            <div class="row">
+            	<header id="masthead" class="site-header">
+            		<div class="site-branding">
+            			<?php j4k_header_logo(); ?>
+            		</div><!-- .site-branding -->
+            
+            		<nav id="site-navigation" class="main-navigation">
+            			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'j4k' ); ?></button>
+            			<?php
+            			wp_nav_menu(
+            				array(
+                                'theme_location' => 'primary',
+            					'menu_id'        => 'primary-menu',
+            				)
+            			);
+            			?>
+            		</nav><!-- #site-navigation -->
+            	</header><!-- #masthead -->
+            </div>
+        </div>
