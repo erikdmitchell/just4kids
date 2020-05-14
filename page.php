9ab2c7a4c5349cfe009cfaded4490eb47c257b34
Erik <erikdmitchell@gmail.com>
2020-05-14 10:05:54 -0400

    added support for wp block cols

--- page.php
+++ page.php
@@ -11,12 +11,12 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-12">
             <?php j4k_theme_post_thumbnail(); ?>
         </div>
     </div>
     <div class="row content">
-        <div class="col-xs-12">
+        <div class="col-12">
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <?php get_template_part( 'content' ); ?>
             <?php endwhile; else : ?>
