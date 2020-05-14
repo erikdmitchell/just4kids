a30f9fd91bd539d1d4858e965ce7614fed5a2698
Erik <erikdmitchell@gmail.com>
2020-05-14 08:07:44 -0400

    moved tempalte into header

--- header.php
+++ header.php
@@ -29,7 +29,22 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <?php get_template_part( 'navbar', 'brand' ); ?>
+
+<div class="j4k-header-wrap ">
+    <?php j4k_header_markup(); ?>
+    
+    <?php if ( display_header_text() ) : ?>
+        <div class="j4k-header-text">
+            <a class="site-title" href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a>
+        
+            <?php $description = get_bloginfo( 'description', 'display' ); ?>
+            <?php if ( $description || is_customize_preview() ) : ?>
+                <p class="site-description"><?php echo $description; ?></p>
+            <?php endif; ?>
+        </div>
+    <?php endif; ?>
+</div>
+
                     </div>
                     <?php j4k_secondary_navigation_setup(); ?>
                     <div class="collapse navbar-collapse primary-menu">
